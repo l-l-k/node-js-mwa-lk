1e9f593c7607d5983b23a7ba3250fae2720e00ba
LeonardKlausmann <leonard1.klausmann@st.oth-regensburg.de>
2016-12-09 18:21:09 +0100

    ADD: text length observer for tweet

---
+++ js/textLengthObserver.js
@@ -0,0 +1,32 @@
+
+var msg; // <textarea> element
+var hint= document.getElementById('charCounter');
+var limit=140;
+
+function updateCounter(e) {
+    var txtField = e.target || e.srcElement;
+    var count = limit - txtField.value.length;
+    if (count < 0) {
+        hint.className = 'error';
+    } else if (count < 11) {
+        hint.className = 'warn';
+    } else {
+        hint.className = 'good';
+    }
+    var txt = 'There are ' + '<b>' + count + '</b>' + ' characters left';
+    hint.innerHTML = txt;
+}
+
+function hideCountere(e) {
+    var txtField = e.target || e.srcElement;
+    var count = txtField.value.length;
+    if (count <= limit) {             
+      hint.className = 'hidden';             
+    }
+}
+
+msg = document.getElementById('message');
+msg.addEventListener('focus', updateCounter, false);
+msg.addEventListener('input', updateCounter, false);
+
+msg.addEventListener('blur', hideCounter, false);
