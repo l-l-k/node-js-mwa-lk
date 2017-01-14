c48b8881274e6b584c0130c83e85bcb5e8c74526
LeonardKlausmann <leonard1.klausmann@st.oth-regensburg.de>
2017-01-14 15:35:24 +0100

    ADD: Postgres DB. Heroku integrated.

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 var http = require('http')
 
+// connect to DATABASE
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
 http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"})
     response.end("Hello World\n")
 }).listen(process.env.PORT)
+
+
+// return all rows in test_table
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+// use a timer
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
