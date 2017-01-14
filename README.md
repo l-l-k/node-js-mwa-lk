0b12ce58cfc059ac403b2655500956c1c62ac0e2
LeonardKlausmann <leonard1.klausmann@st.oth-regensburg.de>
2017-01-14 18:55:01 +0100

    ADD: express package

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# My project's README" 
+# modern web application
+ Single Page Application using Node.js and Heroku 
+
+## Running Locally
+
+Make sure you have installed
+    [Node.js](http://nodejs.org/) and the
+    [Heroku Toolbelt](https://toolbelt.heroku.com/) .
+
+```sh
+$ git clone git@github.com/l-l-k/node-js-mwa-lk.git # or clone your own fork
+$ cd node-js-mwa-lk
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Documentation
--- index.js
+++ index.js
@@ -1,47 +1,77 @@
 var http = require('http')
-
-// connect to DATABASE
 var pg = require('pg');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(process.env.PORT, function () {
+  console.log('Listening on port ' + process.env.PORT + ' !')
+})
+
+//Respond to GET request on the root route (/), the application’s home page:
+app.get('/', function (request, response) {
+  response.send('Hello Ne87ööw World!');
+  //response.render('UI');
+});
+
+
+//Respond to POST request on the root route (/), the application’s home page:
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+//Respond to a PUT request to the /user route:
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+//Respond to a DELETE request to the /user route:
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+// return all rows in test_table
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// });
+
+// use a timer
+// app.get('/times', function(request, response) {
+//     var result = ''
+//     var times = process.env.TIMES || 5
+//     for (i=0; i < times; i++)
+//       result += i + ' ';
+//   response.send(result);
+// });
+
+// ___________________________________________
+// Database initialisation
 
 pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
+//pg.defaults.ssl = false;
+pg.connect(process.env.DATABASE_URL, function (err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World\n")
-}).listen(process.env.PORT)
-
-
-// return all rows in test_table
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
+  // app.get('/', function (request, response) {
+  //   response.send('Connected to postgres! Getting schemas...');
+    //response.render('UI');
   });
-});
+ 
 
-// use a timer
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+  // client
+  //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
+  //   .on('row', function(row) {
+  //     console.log(JSON.stringify(row));
+  //   });
 });
