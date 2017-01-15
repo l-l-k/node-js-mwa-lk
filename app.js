10021b80727bf88ab6d9524d167d0590879f6540
LeonardKlausmann <leonard1.klausmann@st.oth-regensburg.de>
2017-01-15 08:36:14 +0100

    CHANGE: Submit-Paths refined

--- app.js
+++ app.js
@@ -46,16 +46,62 @@ app.post('/', function (req, res) {
   res.send('Got a POST request')
 });
 
-app.post('/signup', function (req, res) {
+app.post('/Submit/signup', function (req, res) {
   console.log('Got a POST request to signup...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
   res.send('Got a POST request')
 });
 
-app.post('/Submit', function (req, res) {
-  console.log('Got a POST request to signup...');
-  console.log(req);
+app.post('/Submit/login', function (req, res) {
+  console.log('Got a POST request to login...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
   res.send('Got a POST request')
 });
+
+app.post('/Submit/account', function (req, res) {
+  console.log('Got a POST request to update account data...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
+  res.send('Got a POST request')
+});
+
+app.post('/Submit/addTweet', function (req, res) {
+  console.log('Got a POST request to add a new tweet...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
+  res.send('Got a POST request')
+});
+
+app.post('/Submit/selectTweets', function (req, res) {
+  console.log('Got a POST request to get specific tweets...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
+  res.send('Got a POST request')
+});
+
+app.post('/Admin/addUser', function (req, res) {
+  console.log('Got a POST request to add a new user...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
+  res.send('Got a POST request')
+});
+
+app.post('/Admin/removeUser', function (req, res) {
+  console.log('Got a POST request to remove an existing user...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
+  res.send('Got a POST request')
+});
+
+app.post('/Admin/statistics', function (req, res) {
+  console.log('Got a POST request to delete tweets...');
+  console.log("param = " + req.params);
+  console.log("query = " + req.query);
+  res.send('Got a POST request')
+});
+
 
 //Respond to a PUT request to the /user route:
 app.put('/user', function (req, res) {
