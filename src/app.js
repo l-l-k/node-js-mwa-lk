70fec640d3b124d3d3d36f76e1a27258f0e77064
LeonardKlausmann <leonard1.klausmann@st.oth-regensburg.de>
2017-05-20 10:31:01 +0200

    Routing refined

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-// import { AuthenticatedStep } from 'authenticated-step';
-
-    // config.addPipelineStep('administer', AuthenticatedStep);
-      // { route: 'admin', name: 'manage', moduleId: 'admin-tools', nav: true, title: 'Signup', settings: { secured:true } },
+import { AuthorizationStep } from 'authorization-step';
 
 export class App {
-  
+
   configureRouter(config, router) {
     this.router = router;
     config.title = 'Postillion';
+   config.addPipelineStep('authorize', AuthorizationStep);
     config.map([
-      { route: '', redirect: 'welcome' },
-      { route: 'welcome', name: 'welcome', moduleId: 'welcome-screen', nav: false },
+      { route: '', redirect: 'home' },
+      { route: 'home', name: 'home', moduleId: 'welcome-screen', nav: false },
       { route: 'login', name: 'login', moduleId: 'login', nav: true, title: 'Login' },
-      { route: 'signup', name: 'signup', moduleId: 'signup', nav: true, title: 'Signup'  },
+      { route: 'signup', name: 'signup', moduleId: 'signup', nav: true, title: 'Signup' },
       { route: 'account', name: 'account', moduleId: 'edit-account', nav: true, title: 'Edit Account' },
-      { route: 'logout', name: 'logout', moduleId: 'logout', nav: true, title: 'Logout'},
+      { route: 'tweet', name: 'tweet', moduleId: 'tweet', nav: true, title: 'Postoffice', settings: { logonRequired: true } },
+      { route: 'management', name: 'management', moduleId: 'management', nav: true, title: 'Management', settings: { restrictedToAdmins:true } },
+      { route: 'logout', name: 'logout', moduleId: 'logout', nav: true, title: 'Logout' }
     ]);
     config.mapUnknownRoutes('not-found');
+    config.fallbackRoute('home');
   }
-  
 }
