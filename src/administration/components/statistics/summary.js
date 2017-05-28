6219cdedbf447a435ff9f4a508f244a7d62f5df2
LeonardKlausmann <leonard1.klausmann@st.oth-regensburg.de>
2017-05-28 11:04:50 +0200

    Statistics-form (summary) refined

--- src/administration/components/statistics/summary.js
+++ src/administration/components/statistics/summary.js
@@ -1,15 +1,25 @@
 import { inject, NewInstance } from 'aurelia-framework';
 import { ValidationController } from 'aurelia-validation';
-import { UserGateway } from './../../services/user-gateway';
-import { User } from './../../models/user';
+import { UserGateway } from './../../../services/user-gateway';
+import { User } from './../../../models/user';
 
 @inject(UserGateway, NewInstance.of(ValidationController))
 export class Statistics {
     constructor(userGateway, validationController) {
         this.userGateway = userGateway;
         this.validationController = validationController;
+        this.user = NewInstance.of(User);
     }
 
     validationFailed = false;
+    displayMessages = false;
 
+    retrieveSummary() {
+        console.log("Retrieve summary : ")
+    }
+
+    emptyGrid() {
+        console.log("Empty summary results : ")
+    }
+    
 }
